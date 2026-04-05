48cc7b236ef3c5a77343b2bb5c80494373506079
Zaira <zairahussain27@gmail.com>
2026-04-05 17:50:01 +0530

    feat: add bubble sort solution in Python

--- two_sum.py
+++ two_sum.py
@@ -28,4 +28,5 @@ def two_sum(nums, target):
 if __name__ == "__main__":
     print(two_sum([2, 7, 11, 15], 9))   # Expected: [0, 1]
     print(two_sum([3, 2, 4], 6))         # Expected: [1, 2]
-    print(two_sum([3, 3], 6))            # Expected: [0, 1]
+    print(two_sum([3, 2, 3], 6))         # Expected: [0, 2]
+    print(two_sum([2, 2, 2], 4))         # Expected: [0, 1]
